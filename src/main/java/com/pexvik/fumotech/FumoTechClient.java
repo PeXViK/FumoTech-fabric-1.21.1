f063146e91a609079f23f03c9090ff232bb55163
PeXViK <pexik.pvp@gmail.com>
2026-01-06 16:12:23 +0300

    project is created

---
+++ src/main/java/com/pexvik/fumotech/FumoTechClient.java
@@ -0,0 +1,11 @@
+package com.pexvik.fumotech;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FumoTechClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/pexvik/fumotech/FumoTechDataGenerator.java
@@ -0,0 +1,11 @@
+package com.pexvik.fumotech;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FumoTechDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
